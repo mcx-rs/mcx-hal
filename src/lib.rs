c21fc88bc12da3cd56cc7ae8f9175bdfda2307f6
Logiase Song <logiase.song@nxp.com>
2024-03-08 11:49:58 +0800

    lib: remove unused macro

--- src/lib.rs
+++ src/lib.rs
@@ -27,22 +27,3 @@ pub enum HalError {
 }
 
 impl sealed::Sealed for HalError {}
-
-macro_rules! is_trait {
-    ($name:ty, $trait_name:path) => {{
-        trait __InnerMarkerTrait {
-            fn __is_trait_inner_method() -> bool {
-                false
-            }
-        }
-        struct __TraitTest<T>(T);
-        impl<T: $trait_name> __TraitTest<T> {
-            fn __is_trait_inner_method() -> bool {
-                true
-            }
-        }
-        impl<T> __InnerMarkerTrait for __TraitTest<T> {}
-        __TraitTest::<$name>::__is_trait_inner_method()
-    }};
-}
-pub(crate) use is_trait;
