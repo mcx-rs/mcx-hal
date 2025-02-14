a5e4cc7eadc7e2194b30787a05dbbfb70644051c
Logiase Song <logiase.song@gmail.com>
2025-02-14 17:04:25 +0800

    Create cargo project.

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+//! Hardware Abstraction Layer for NXP MCX Series MCUs.
+
+#![no_std]
+
+pub use mcx_pac as pac;
+
+pub mod prelude {}
+
+#[derive(Clone, Copy, Debug)]
+pub enum HalError {
+    Fail,
+    ReadOnly,
+    OutOfRange,
+    InvalidValue,
+    Timeout,
+    Busy,
+}
+
+mod private {
+    /// Sealed trait to protect crate traits not implemented by outside crate code.
+    pub trait Sealed {}
+}
