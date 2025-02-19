661b9293d1d8417bfe26a44323b28666df757554
Logiase Song <logiase.song@gmail.com>
2025-02-19 14:50:17 +0800

    Move Error to specific mod.

--- src/lib.rs
+++ src/lib.rs
@@ -4,23 +4,16 @@
 
 pub use mcx_pac as pac;
 
+pub mod device;
 pub mod gpio;
 pub mod port;
+pub mod scg;
 pub mod syscon;
-
-pub mod prelude {}
-
-#[derive(Clone, Copy, Debug)]
-pub enum HalError {
-    Fail,
-    ReadOnly,
-    OutOfRange,
-    InvalidValue,
-    Timeout,
-    Busy,
-}
 
 mod private {
     /// Sealed trait to protect crate traits not implemented by outside crate code.
     pub trait Sealed {}
 }
+
+#[cfg(not(any(feature = "mcxa", feature = "mcxn")))]
+compile_error!("Please select one device");
